b81ecc555fca00dfbcfbaa8eadf50be64da0b04d
Vishwa <vishwa4evr@gmail.com>
2020-09-28 16:17:37 +0530

    added the format

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p >Working..!!</p>
+  
+      </div>
+    );
+  }
+}
+
+export default App;
