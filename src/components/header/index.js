f64840e175a2c7b3645f53b679ff6f110e6301cb
Vishwa <vishwa4evr@gmail.com>
2020-10-08 19:23:49 +0530

    added more functionality to browse page -card,video setup

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,7 +9,14 @@ import {
   Text,
   FeatureCallOut,
   Link,
-  Group
+  Group,
+  Profile,
+  Picture,
+  Dropdown,
+  Search,
+  SearchIcon,
+  SearchInput,
+  PlayButton
 } from './styles/header';
 
 function Header({ bg = true, children, ...restProps }) {
@@ -28,7 +35,10 @@ Header.Feature = function HeaderFeature({ children, ...restProps }) {
   return <Feature>{children}</Feature>;
 };
 
-Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
   return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
 };
 
@@ -60,45 +70,44 @@ Header.Group = function HeaderGroup({ children, ...restProps }) {
   return <Group {...restProps}>{children}</Group>;
 };
 
-// Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
-//   const [searchActive, setSearchActive] = useState(false);
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
 
-//   return (
-//     <Search {...restProps}>
-//       <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
-//         <img src="/images/icons/search.png" alt="Search" />
-//       </SearchIcon>
-//       <SearchInput
-//         value={searchTerm}
-//         onChange={({ target }) => setSearchTerm(target.value)}
-//         placeholder="Search films and series"
-//         active={searchActive}
-//         data-testid="search-input"
-//       />
-//     </Search>
-//   );
-// };
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
 
-// Header.Profile = function HeaderProfile({ children, ...restProps }) {
-//   return <Profile {...restProps}>{children}</Profile>;
-// };
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
 
+Header.Search = function HeaderSearch({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}) {
+  const [searchActive, setSearchActive] = useState(false);
 
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setSearchActive(searchActive => !searchActive)}
+        data-testid="search-click"
+      >
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={searchActive}
+        data-testid="search-input"
+      />
+    </Search>
+  );
+};
 
-// Header.Picture = function HeaderPicture({ src, ...restProps }) {
-//   return <Picture {...restProps} src={`/images/users/${src}.png`} />;
-// };
-
-// Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
-//   return <Dropdown {...restProps}>{children}</Dropdown>;
-// };
-
-
-
-// Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
-//   return <PlayButton {...restProps}>{children}</PlayButton>;
-// };
-
-
-
-
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
+};
--- src/container/browser.js
+++ src/container/browser.js
@@ -1,13 +1,17 @@
 import React, { useContext, useState, useEffect } from 'react';
 import SelectProfileContainer from './profile';
 import { FirebaseContext } from '../context/firebase';
-import { Loading, Header } from '../components';
+import { Card, Loading, Header, Player } from '../components';
 import logo from '../logo.svg';
 import * as ROUTES from '../constants/route';
+import { FooterContainer } from './footer';
 
 export function BrowserContainer({ slides }) {
+  const [category, setCategory] = useState('series');
+  const [searchTerm, setSearchTerm] = useState('');
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
+  const [slideRows, setSlideRows] = useState([]);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
   useEffect(() => {
@@ -16,6 +20,11 @@ export function BrowserContainer({ slides }) {
       setLoading(false);
     }, 3000);
   }, [profile.displayName]);
+
+  useEffect(() => {
+    setSlideRows(slides[category]);
+  }, [slides, category]);
+
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
@@ -24,8 +33,38 @@ export function BrowserContainer({ slides }) {
         <Header.Frame>
           <Header.Group>
             <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-            <Header.TextLink>Series</Header.TextLink>
-            <Header.TextLink>Films</Header.TextLink>
+            <Header.TextLink
+              active={category === 'series' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === 'films' ? 'true' : 'false'}
+              onClick={() => setCategory('films')}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+              <Header.Group>
+                <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                  Sign out
+                </Header.TextLink>
+              </Header.Group>
+            </Header.Profile>
           </Header.Group>
         </Header.Frame>
         <Header.Feature>
@@ -37,8 +76,37 @@ export function BrowserContainer({ slides }) {
             he projects in a futile attempt to feel like he's part of the world
             around him.
           </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+      {/* BELOW HEADER GOES CARD */}
+      <Card.Group>
+        {slideRows.map(slideItem => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map(item => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player>
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
+      <FooterContainer />
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
